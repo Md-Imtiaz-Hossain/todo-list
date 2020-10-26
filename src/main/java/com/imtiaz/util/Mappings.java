4d9836828a4a4b9bb1e35defc6f659de03d3b504
Imtiaz <imtiaz005123@gmail.com>
2020-10-26 11:32:16 +0600

    For web page data view start working with (TodoItemController.java) class, set @GetMapping and java class return value purpose we created (Mappings.java) and (ViewNames.java) class.

---
+++ src/main/java/com/imtiaz/util/Mappings.java
@@ -0,0 +1,12 @@
+package com.imtiaz.util;
+
+public final class Mappings {
+
+    // === constance ===
+    public static final String ITEMS = "items";
+
+
+    // === constructor ===
+    private Mappings() {
+    }
+}
---
+++ src/main/java/com/imtiaz/util/ViewNames.java
@@ -0,0 +1,12 @@
+package com.imtiaz.util;
+
+public final class ViewNames {
+
+    // === constance ===
+    public static final String ITEMS_LIST = "items_list";
+
+
+    // === constructor ===
+    private ViewNames() {
+    }
+}
